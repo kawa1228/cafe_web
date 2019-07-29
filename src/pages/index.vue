704ee51a5be5224b60445c46d49949bf30853202
kawa1228 <sxa.1105bo@gmail.com>
2019-07-29 17:49:13 +0900

    fix: Vueのデフォルト表示から変更

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,35 +1,18 @@
 <template>
   <div class="container">
-    <div>
       <CreeksLogo />
-      <h1 class="title">
-        cafe_web
-      </h1>
-      <h2 class="subtitle">
-        My laudable Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <nuxt-link to="/">ABOUT</nuxt-link>
+      <nuxt-link to="/">MENU</nuxt-link>
+      <nuxt-link to="/">PHOTO</nuxt-link>
+      <img src="/img/CafeAppearance.jpg" alt="">
+      <div>
+        <p>掛川城や図書館の近く、街中なのに木々に囲まれ、</p>
+        <p>森の中にいるような雰囲気のカフェです</p>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 import CreeksLogo from '~/assets/svg/CreeksLogo.svg'
 
 export default {
@@ -39,35 +22,5 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="scss" src="./style.scss" scoped>
 </style>
