8775969d2a5a60ceffa1d1049e1766471dac0b95
kawa1228 <sxa.1105bo@gmail.com>
2019-07-30 18:15:23 +0900

    feat: ヘッダーをコンポーネントに切り分け

---
+++ src/components/Header/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="home__top">
+      <h1 class="home__top__title">
+        <CreeksLogo class="home__top__title__logo"/>
+      </h1>
+      <div class="home__top__lang">
+        <nuxt-link to="/">JP</nuxt-link>
+        <nuxt-link :class="selectedStyle" to="/">EN</nuxt-link>
+      </div>
+    </div>
+    <Navigation/>
+  </div>
+</template>
+
+<script>
+import CreeksLogo from '~/assets/svg/CreeksLogo.svg'
+import Navigation from '~/components/Navigation'
+
+export default {
+  components: {
+    CreeksLogo,
+    Navigation
+  },
+  computed: {
+    selectedStyle() {
+      return 'home__top__lang--selected'
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped>
+</style>
