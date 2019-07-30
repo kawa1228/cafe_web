a46a49f909a76ace827ad4d93621d29b31fef7be
kawa1228 <sxa.1105bo@gmail.com>
2019-07-30 17:46:09 +0900

    fix: トップページのレイアウト修正

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <div class="home__top">
-      <CreeksLogo class="home__top__logo"/>
+      <h1 class="home__top__title">
+        <CreeksLogo class="home__top__title__logo"/>
+      </h1>
       <div class="home__top__lang">
         <nuxt-link to="/">JP</nuxt-link>
         <nuxt-link :class="selectedStyle" to="/">EN</nuxt-link>
@@ -14,10 +16,14 @@
     </nav>
     <article class="home__contents">
       <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcreekscafe&tabs=timeline&width=184&height=275&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="184" height="275" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-      <section class="home__contents__about">
-        <img class="home__contents__about__img" src="/img/CafeAppearance.jpg" alt="カフェ外観">
-        <p class="home__contents__about__text">掛川城や図書館の近く、街中なのに木々に囲まれ、</p>
-        <p class="home__contents__about__text">森の中にいるような雰囲気のカフェです。</p>
+      <section class="home__contents__main">
+        <div class="home__contents__main__img">
+          <img src="/img/CafeAppearance.jpg" alt="カフェ外観">
+        </div>
+        <div class="home__contents__main__text">
+          <p>掛川城や図書館の近く、街中なのに木々に囲まれ、</p>
+          <p>森の中にいるような雰囲気のカフェです。</p>
+        </div>
       </section>
     </article>
   </div>
