7874cc87cbfc6d4d238908ecbabd0568399ab122
kawa1228 <sxa.1105bo@gmail.com>
2019-07-30 18:15:52 +0900

    feat: ナビゲーションをコンポーネントに切り分け

---
+++ src/components/Navigation/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="home__menu">
+    <nuxt-link class="home__menu__tab" to="/">ABOUT</nuxt-link>
+    <nuxt-link class="home__menu__tab" to="/">MENU</nuxt-link>
+    <nuxt-link class="home__menu__tab" to="/">PHOTO</nuxt-link>
+  </nav>
+</template>
+
+<style lang="scss" src="./style.scss" scoped>
+</style>
