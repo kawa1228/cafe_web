956bf71ec2f253dc67d9e96f7daf80a7a0eed178
kawa1228 <sxa.1105bo@gmail.com>
2019-07-30 14:46:04 +0900

    feat: フッターのコンポーネントを新規作成

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer class="footer">
+    <p class="footer__text">〒436-0079 静岡県掛川市掛川1070-12</p>
+    <FaceBookLogo class="footer__logo"/>
+  </footer>
+</template>
+
+<script>
+import FaceBookLogo from '~/assets/svg/FaceBookLogo.svg'
+
+export default {
+  components: {
+    FaceBookLogo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  height: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #000;
+
+  &__text {
+    font-family: $default-jp-font-family;
+    color: #fff;
+    font-size: 12px;
+  }
+
+  &__logo {
+    width: 100%;
+    margin-top: 9px;
+  }
+}
+</style>
