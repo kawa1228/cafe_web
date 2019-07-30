83eb497e86cfa5f42bdc6bd6f623451aee75cb6e
kawa1228 <sxa.1105bo@gmail.com>
2019-07-30 18:30:58 +0900

    fix: トップページのhtml構造も修正

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="home">
-    <article class="home__contents">
-      <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcreekscafe&tabs=timeline&width=184&height=275&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="184" height="275" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-      <section class="home__contents__main">
-        <div class="home__contents__main__img">
-          <img src="/img/CafeAppearance.jpg" alt="カフェ外観">
-        </div>
-        <div class="home__contents__main__text">
-          <p>掛川城や図書館の近く、街中なのに木々に囲まれ、</p>
-          <p>森の中にいるような雰囲気のカフェです。</p>
-        </div>
-      </section>
-    </article>
+  <div class="contents">
+    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcreekscafe&tabs=timeline&width=184&height=275&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="184" height="275" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    <section class="contents__main">
+      <div class="contents__main__img">
+        <img src="/img/CafeAppearance.jpg" alt="カフェ外観">
+      </div>
+      <div class="contents__main__text">
+        <p>掛川城や図書館の近く、街中なのに木々に囲まれ、</p>
+        <p>森の中にいるような雰囲気のカフェです。</p>
+      </div>
+    </section>
   </div>
 </template>
 
