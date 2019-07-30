acd87684aac17d0d8539578b53ce141d164b45d4
kawa1228 <sxa.1105bo@gmail.com>
2019-07-30 18:16:53 +0900

    fix: コンポーネントに切り出した部分のページを削除

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,5 @@
 <template>
   <div class="home">
-    <div class="home__top">
-      <h1 class="home__top__title">
-        <CreeksLogo class="home__top__title__logo"/>
-      </h1>
-      <div class="home__top__lang">
-        <nuxt-link to="/">JP</nuxt-link>
-        <nuxt-link :class="selectedStyle" to="/">EN</nuxt-link>
-      </div>
-    </div>
-    <nav class="home__menu">
-      <nuxt-link class="home__menu__tab" to="/">ABOUT</nuxt-link>
-      <nuxt-link class="home__menu__tab" to="/">MENU</nuxt-link>
-      <nuxt-link class="home__menu__tab" to="/">PHOTO</nuxt-link>
-    </nav>
     <article class="home__contents">
       <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcreekscafe&tabs=timeline&width=184&height=275&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="184" height="275" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
       <section class="home__contents__main">
@@ -28,21 +14,6 @@
     </article>
   </div>
 </template>
-
-<script>
-import CreeksLogo from '~/assets/svg/CreeksLogo.svg'
-
-export default {
-  components: {
-    CreeksLogo,
-  },
-  computed: {
-    selectedStyle() {
-      return 'home__top__lang--selected'
-    }
-  }
-}
-</script>
 
 <style lang="scss" src="./style.scss" scoped>
 </style>
