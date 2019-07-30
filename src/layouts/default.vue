c8dd1769ae2377a88428501e6f8ebb3259f96e17
kawa1228 <sxa.1105bo@gmail.com>
2019-07-30 18:16:32 +0900

    fix: デフォルトレイアウトにヘッダーを追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="cafe-web">
+    <Header/>
     <nuxt />
     <Footer class="cafe-web__footer"/>
   </div>
 </template>
 
 <script>
+import Header from '~/components/Header'
 import Footer from '~/components/Footer'
 
 export default {
   components: {
+    Header,
     Footer
   }
 }
