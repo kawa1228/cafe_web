66b2200ffacb825009f7caa6a0521dae33b2b4a9
kawa1228 <sxa.1105bo@gmail.com>
2019-08-05 19:50:23 +0900

    fix: aboutページを修正。見た目はOKそう

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -9,24 +9,30 @@
     <div class="about__map">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d560.0148341656708!2d138.01663924115505!3d34.77540776617764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601af999c28ea8e5%3A0x92129273165cea45!2sCreek&#39;s+Caf%C3%A9!5e0!3m2!1sja!2sjp!4v1564480007835!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
-    <dl>
-      <div>
-        <dt>住所：</dt>
-        <dd>〒436-0079　静岡県掛川市掛川1070-12</dd>
-        <dd>JR東海道本線「掛川駅」より徒歩10分</dd>
+    <dl class="about__main">
+      <div class="about__main__list">
+        <dt class="about__main__list__term">住所：</dt>
+        <div class="about__main__list__description">
+          <dd>〒436-0079　静岡県掛川市掛川1070-12</dd>
+          <dd>JR東海道本線「掛川駅」より徒歩10分</dd>
+        </div>
       </div>
-      <div>
-        <dt>営業時間：</dt>
-        <dd>facebookページをご確認ください。</dd>
-        <dd>※通常は(水)～(土)、夕方6時からの営業です。</dd>
+      <div class="about__main__list">
+        <dt class="about__main__list__term">営業時間：</dt>
+        <div class="about__main__list__description">
+          <dd>facebookページをご確認ください。</dd>
+          <dd>※通常は(水)～(土)、夕方6時からの営業です。</dd>
+        </div>
       </div>
-      <div>
-        <dt>ご予約・お問い合わせ：</dt>
-        <dd>facebookページからご連絡ください。</dd>
+      <div class="about__main__list">
+        <dt class="about__main__list__term">ご予約・お問い合わせ：</dt>
+        <div class="about__main__list__description">
+          <dd>facebookページからご連絡ください。</dd>
+        </div>
       </div>
-      <ul>
-        <li>- 写真講座を開催しています。</li>
-        <li>- 日中は貸しスペースとしてもご利用いただけます。</li>
+      <ul class="about__main__supplement">
+        <li>写真講座を開催しています。</li>
+        <li>日中は貸しスペースとしてもご利用いただけます。</li>
       </ul>
     </dl>
   </div>
