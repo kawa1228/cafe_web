e51079927ee41736ed0e8aaa38499b0216f851a7
kawa1228 <sxa.1105bo@gmail.com>
2019-08-05 19:50:54 +0900

    feat: menuページにhtmlを追加

--- src/pages/menu/index.vue
+++ src/pages/menu/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>menu</div>
+  <div class="menu">
+    <dl>
+      <dt>前菜類（サラダ等）</dt>
+      <dt>野菜料理</dt>
+      <dt>魚料理</dt>
+      <dt>肉料理</dt>
+      <dt>パスタ</dt>
+      <dt>スイーツ</dt>
+      <dt>ドリンク</dt>
+      <dd>掛川深蒸し茶 / コーヒー / 紅茶 / カフェオレ / ノンアルコールビール / ジンジャーエール / ペプシ など</dd>
+      <dt>お酒</dt>
+      <dd>　ビール / 日本酒 / 焼酎（芋・麦・米） / 梅酒 / ワイン / ウイスキー / ハイボール など</dd>
+    </dl>
+  </div>
 </template>
 
 <style lang="scss" src="./style.scss" scoped>
