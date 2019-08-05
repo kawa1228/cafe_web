7c4a5785ef9b324196e81b5f62fe6e85195d7a49
kawa1228 <sxa.1105bo@gmail.com>
2019-08-05 20:24:35 +0900

    fix: menuページのcssを追加

--- src/pages/menu/index.vue
+++ src/pages/menu/index.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="menu">
-    <dl>
-      <dt>前菜類（サラダ等）</dt>
-      <dt>野菜料理</dt>
-      <dt>魚料理</dt>
-      <dt>肉料理</dt>
-      <dt>パスタ</dt>
-      <dt>スイーツ</dt>
-      <dt>ドリンク</dt>
-      <dd>掛川深蒸し茶 / コーヒー / 紅茶 / カフェオレ / ノンアルコールビール / ジンジャーエール / ペプシ など</dd>
-      <dt>お酒</dt>
-      <dd>　ビール / 日本酒 / 焼酎（芋・麦・米） / 梅酒 / ワイン / ウイスキー / ハイボール など</dd>
+    <dl class="menu__list">
+      <dt class="menu__list__term">前菜類（サラダ等）</dt>
+      <dt class="menu__list__term">野菜料理</dt>
+      <dt class="menu__list__term">魚料理</dt>
+      <dt class="menu__list__term">肉料理</dt>
+      <dt class="menu__list__term">パスタ</dt>
+      <dt class="menu__list__term">スイーツ</dt>
+      <dt class="menu__list__term">ドリンク</dt>
+      <dd class="menu__list__description">掛川深蒸し茶 / コーヒー / 紅茶 / カフェオレ /</dd>
+      <dd class="menu__list__description">ノンアルコールビール / ジンジャーエール / ペプシ など</dd>
+      <dt class="menu__list__term">お酒</dt>
+      <dd class="menu__list__description">ビール / 日本酒 / 焼酎（芋・麦・米） / 梅酒 / ワイン /</dd>
+      <dd class="menu__list__description">ウイスキー / ハイボール など</dd>
     </dl>
+    <p class="menu__attention">メニューはその日によって変わることがあります。</p>
   </div>
 </template>
 
