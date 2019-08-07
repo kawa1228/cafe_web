572ad7c9136c0415825fada3d12582b95562fa53
kawa1228 <sxa.1105bo@gmail.com>
2019-08-07 17:42:17 +0900

    fix: photoページを作成

--- src/pages/photo/index.vue
+++ src/pages/photo/index.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>photo</div>
+  <div class="photo">
+    <p>オーナーの写真作品です。</p>
+    <p>写真講座も開催しています。</p>
+    <section class="photo__architecture">
+      <div class="photo__architecture__title">
+        <h2>Architecture</h2>
+      </div>
+      <div class="photo__architecture__contents">
+        <img src="/img/Architecture01.jpg" alt="建築写真のサンプル">
+        <img src="/img/Architecture02.jpg" alt="建築写真のサンプル">
+        <img src="/img/Architecture03.jpg" alt="建築写真のサンプル">
+      </div>
+    </section>
+    <section class="photo__portrait">
+      <div class="photo__portrait__title">
+        <h2>Portrait</h2>
+      </div>
+      <div class="photo__portrait__contents">
+        <img src="/img/Portrait01.jpg" alt="人物写真のサンプル">
+        <img src="/img/Portrait02.jpg" alt="人物写真のサンプル">
+        <img src="/img/Portrait03.jpg" alt="人物写真のサンプル">
+      </div>
+    </section>
+    <section class="photo__auther">
+      <h2 class="photo__auther__name">小川博彦（おがわ ひろひこ）</h2>
+      <div class="photo__auther__introduction">
+        <p>1957年富士市生まれ。</p>
+        <p>プロカメラマンとして39年。撮影は、商品、人物、風景写真など多岐にわたる。</p>
+        <p>「掛川市市勢要覧2009」「新幹線掛川駅開業20周年記念誌」など、掛川近郊での撮影も多い。</p>
+        <p>掛川市生涯学習講座「はじめてのフォトレッスン」、掛川ライフスタイルデザインカレッジ「ネイチャーフォトグラフィー」講師。『サイトフィッシングの戦術（つり人社刊）』の著者・撮影者でもある。</p>
+        <p>2008年より掛川市倉真の古民家に居を構え、2014年より掛川市のまちなかにスタジオ兼のカフェを開業する。</p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style lang="scss" src="./style.scss" scoped>
