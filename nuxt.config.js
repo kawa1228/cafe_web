122ebaf48fc45bb34e4f2264cb083ce8c4ec1f48
kawa1228 <sxa.1105bo@gmail.com>
2019-08-08 23:22:29 +0900

    feat: image previewerのpluginを新規作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '~/plugins/vue-image-previewer.js', ssr: false }
   ],
   /*
   ** Nuxt.js dev-modules
---
+++ src/plugins/vue-image-previewer.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import VuePreviewer from 'vue-image-previewer';
+
+Vue.component('vue-previewer', VuePreviewer)
