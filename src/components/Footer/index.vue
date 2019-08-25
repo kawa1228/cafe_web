f6203af14be50f8523bd1bd9f5c9e57ed209be19
kawa1228 <sxa.1105bo@gmail.com>
2019-08-26 02:30:50 +0900

    fix: facebookへのリンクをにtarget_blankをつける

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
     <p class="footer__text">〒436-0079 静岡県掛川市掛川1070-12</p>
-    <a href="https://www.facebook.com/creekscafe">
+    <a target="_blank" href="https://www.facebook.com/creekscafe">
       <FaceBookLogo class="footer__logo"/>
     </a>
   </footer>
