d999da366cd10f927ba32c1bf3b97ac06235e75f
kawa1228 <sxa.1105bo@gmail.com>
2019-08-26 00:32:47 +0900

    fix: sp版menuの作成

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -4,8 +4,14 @@
       <CreeksLogo class="header__logo"/>
     </nuxt-link>
     <div class="header__menu">
-      <MenuIcon class="header__menu__icon"/>
-      <span class="header__menu__text">Menu</span>
+      <div class="header__menu--close" v-if="isOpenedMenu">
+        <CloseIcon @click="onClickClose" class="header__menu--close__icon"/>
+        <span class="header__menu--close__text">Close</span>
+      </div>
+      <div v-else class="header__menu--open">
+        <MenuIcon @click="onClickMenu" class="header__menu--open__icon"/>
+        <span class="header__menu--open__text">Menu</span>
+      </div>
     </div>
     <div class="header__lang">
       <nuxt-link to="/">JP</nuxt-link>
@@ -16,16 +22,32 @@
 
 <script>
 import CreeksLogo from '~/assets/svg/CreeksLogo.svg'
+import CloseIcon from '~/assets/svg/CloseIcon.svg'
 import MenuIcon from '~/assets/svg/MenuIcon.svg'
 
 export default {
+  props: {
+    isOpenedMenu: {
+      type: Boolean,
+      required: true
+    }
+  },
   components: {
     CreeksLogo,
+    CloseIcon,
     MenuIcon
   },
   computed: {
     selectedStyle() {
       return 'header__top__lang--selected'
+    }
+  },
+  methods: {
+    onClickMenu() {
+      this.$emit('open-menu')
+    },
+    onClickClose() {
+      this.$emit('close-menu')
     }
   }
 }
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="default-layout">
-    <Header ref="header"/>
+    <Header ref="header" :isOpenedMenu="isOpenedMenu" @open-menu="openedMenu" @close-menu="closedMenu"/>
+    <!-- TODO: 一連の処理を含めコンポーネントにわける -->
+    <div v-if="isOpenedMenu" class="default-layout__navi--sp">
+      <ul class="default-layout__navi--sp__list">
+        <li class="default-layout__navi--sp__list__item" v-for="(item, i) in menuList" :key="`navi--sp__list__item--${i}`">
+          <nuxt-link @click.native="closedMenu" :to="item.link">{{ item.title }}</nuxt-link>
+        </li>
+        <div class="default-layout__navi--sp__list__access">
+          <p>〒436-0079　静岡県掛川市掛川1070-12</p>
+          <p>JR東海道本線「掛川駅」より徒歩10分</p>
+        </div>
+      </ul>
+    </div>
     <div class="default-layout__navi">
       <transition name="title-fade">
         <h1 v-if="isShowTitle" class="default-layout__navi__title">Creek's Cafe</h1>
@@ -16,6 +28,7 @@
 import Header from '~/components/Header'
 import Navigation from '~/components/Navigation'
 import Footer from '~/components/Footer'
+import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 
 export default {
   components: {
@@ -25,20 +38,49 @@ export default {
   },
   data() {
     return {
-      isShowTitle: false
+      isShowTitle: false,
+      isOpenedMenu: false,
+      menuList: [
+        {title: 'HOME', link: '/'},
+        {title: 'ABOUT', link: '/about'},
+        {title: 'MENU', link: '/menu'},
+        {title: 'PHOTO', link: '/photo'}
+      ]
+    }
+  },
+  watch: {
+    isOpenedMenu() {
+      this.onChangeScrollLock()
     }
   },
   mounted() {
     window.addEventListener('scroll', this.handleScroll, { passive: true });
+    this.onChangeScrollLock();
   },
   destroyed() {
     window.removeEventListener('scroll', this.handleScroll);
+    this.isOpenedMenu = false;
+    this.onChangeScrollLock();
   },
   methods: {
     handleScroll() {
       const header = this.$refs['header'].$el
       const rect = header.getBoundingClientRect()
       this.isShowTitle = (rect.height + rect.top) <= 15
+    },
+    openedMenu() {
+      this.isOpenedMenu = true
+    },
+    closedMenu() {
+      this.isOpenedMenu = false
+    },
+    onChangeScrollLock() {
+      const elem = document.querySelector('.default-layout')
+      if (this.isOpenedMenu) {
+        disableBodyScroll(elem)
+      } else {
+        clearAllBodyScrollLocks()
+      }
     }
   }
 }
