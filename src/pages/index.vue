b85b0f5625d30801a5916c43f58934b16cf4cf6b
kawa1228 <sxa.1105bo@gmail.com>
2019-08-25 17:55:24 +0900

    style: tablet幅のときのレイアウトを追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="contents">
-    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcreekscafe&tabs=timeline&width=184&height=275&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="184" height="275" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    <div class="contents__facebook">
+      <iframe class="contents__facebook__iframe" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fm.facebook.com%2Fcreekscafe&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="184" height="275" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    </div>
     <section class="contents__main">
       <div class="contents__main__img">
         <img src="/img/CafeAppearance.jpg" alt="カフェ外観">
@@ -12,6 +14,19 @@
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    const tabletWidth = 995
+    if (window.innerWidth < tabletWidth ) {
+      // タブレット以下でfacebookの幅を変更
+      const facebook = document.querySelector('.contents__facebook__iframe')
+      facebook.setAttribute('width', "500")
+    }
+  }
+}
+</script>
 
 <style lang="scss" src="./style.scss" scoped>
 </style>
