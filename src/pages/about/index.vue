69ea109714638a88b5b9aa075dc3357cdc0a191a
kawa1228 <sxa.1105bo@gmail.com>
2019-08-25 21:51:54 +0900

    style: aboutページのsp版余白を調整

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -3,8 +3,7 @@
     <div class="about__text">
       <p>イタリアンを中心に、お料理・お酒等をリーズナブルなお値段でご提供しています。</p>
       <p>本業はプロカメラマンのため、写真の仕事があるときはお休みになります。</p>
-      <p>いつやっているかわからないようなカフェですが、</p>
-      <p>もしよかったら足をのばしてみてください。</p>
+      <p>いつやっているかわからないようなカフェですが、もしよかったら足をのばしてみてください。</p>
     </div>
     <div class="about__map">
       <iframe class="about__map__iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d560.0148341656708!2d138.01663924115505!3d34.77540776617764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601af999c28ea8e5%3A0x92129273165cea45!2sCreek&#39;s+Caf%C3%A9!5e0!3m2!1sja!2sjp!4v1564480007835!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
