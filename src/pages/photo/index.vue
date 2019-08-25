2a5a09b67068ff31fafcaf5ab5b8879669280a83
kawa1228 <sxa.1105bo@gmail.com>
2019-08-26 01:24:38 +0900

    chore: vue-slickを導入

--- src/pages/photo/index.vue
+++ src/pages/photo/index.vue
@@ -7,6 +7,13 @@
         <h2>Architecture</h2>
       </div>
       <div class="photo__architecture__contents">
+        <no-ssr>
+          <slick ref="slick" :options="slickOptions">
+            <img src="/img/Architecture01.jpg" alt="">
+            <img src="/img/Architecture02.jpg" alt="">
+            <img src="/img/Architecture03.jpg" alt="">
+          </slick>
+        </no-ssr>
         <no-ssr placeholder="Loading...">
           <vue-previewer v-for="(item, i) in ArchitectureImg" :images="item.images" mode="image" :options="item.options" :key="`architecture__img--key-${i}`"/>
         </no-ssr>
@@ -40,6 +47,13 @@
 
 <script>
 export default {
+  data() {
+    return {
+      slickOptions: {
+        arrows: false
+      }
+    }
+  },
   computed: {
     ArchitectureImg() {
       return [
