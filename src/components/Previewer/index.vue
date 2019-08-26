97ad7c8e2359a3001dd3b4ae46011a0814d9b8cf
kawa1228 <sxa.1105bo@gmail.com>
2019-08-26 19:57:21 +0900

    feat: preview用コンポーネントを作成

---
+++ src/components/Previewer/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="previewer">
+    <no-ssr placeholder="Loading...">
+      <vue-previewer v-for="(item, i) in previewItems" :images="item.images" mode="image" :options="item.options" :key="`previewer__img--key-${i}`"/>
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    previewItems: {
+      images: {
+        type: Array,
+        default: null
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped>
+</style>
